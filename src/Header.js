6a003c95cbd39946c3fe77bd7b4dc9b0d5d5c07c
Pauline Remi <pauline.remi@lapiscine.pro>
2023-11-08 10:12:23 +0100

    add if isUserLogged

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,18 @@
 function Header () {
+    const isUserLogged = true
+
     return (
         <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Product</li>
+                    <li>Footer</li>
+                </ul>
+            </nav>
+            {isUserLogged ? <li>Pauline Rémi</li> : <li>Veuillez vous connecter</li>}
             <h1>Nouvelles sneakers à la mode</h1>
+
         </header>
     )
 }
